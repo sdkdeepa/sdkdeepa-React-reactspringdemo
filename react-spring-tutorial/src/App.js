f3e8e705220099dd03de95129bf990f530558c79
sdkdeepa <deepa94086@gmail.com>
2022-11-06 19:49:49 -0800

    First commit
    First commit

---
+++ react-spring-tutorial/src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Translate from "./Translate"
+import BarGraph from "./BarGraph"
+
+function App() {
+  return (
+    <div>
+      {/* <Translate /> */}
+      <BarGraph />
+    </div>
+  )
+}
+
+export default App
---
+++ react-spring-tutorial/src/BarGraph.js
@@ -0,0 +1,64 @@
+import React from "react"
+import { useSprings, animated } from "react-spring"
+
+const bars = [
+  {
+    key: "bar1",
+    color: "green",
+    from: {
+      width: "0px",
+    },
+    to: {
+      width: "100px",
+    },
+    config: {
+      mass: 20,
+    },
+  },
+  {
+    key: "bar2",
+    color: "blue",
+    from: {
+      width: "0px",
+    },
+    to: {
+      width: "250px",
+    },
+    config: {
+      mass: 30,
+    },
+  },
+  {
+    key: "bar3",
+    color: "red",
+    from: {
+      width: "0px",
+    },
+    to: {
+      width: "150px",
+    },
+    config: {
+      mass: 13,
+    },
+  },
+]
+
+const BarGraph = () => {
+  const springs = useSprings(
+    bars.length,
+    bars.map(({ color, key, ...config }) => config)
+  )
+
+  return springs.map((spring, index) => (
+    <animated.div
+      style={{
+        ...spring,
+        height: "20px",
+        marginBottom: "10px",
+        backgroundColor: bars[index].color,
+      }}
+    />
+  ))
+}
+
+export default BarGraph
